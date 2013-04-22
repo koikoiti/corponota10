b11d6a3dba7a3a8db39b3ba408dca724cf9ed66b
Israel - Note <israelcbj@gmail.com>
2013-04-22 19:06:31 -0300

    inicio

--- shopping/functions/banco-inicio.php
+++ shopping/functions/banco-inicio.php
@@ -1,4 +1,35 @@
 <?php
 	class bancoinicio extends banco{
 		
+	#Funcao que lista os Docs
+		function ListaProdutosInicio($Auxilio){
+			$Banco_Vazio = "Banco esta Vazio";
+			#Query Busca Docs
+			$Sql = "Select * from s_produto";
+			$result = parent::Execute($Sql);
+			$num_rows = parent::Linha($result);
+			
+			#Monta no Html a Listagem
+			if ($num_rows){
+				while( $rs = mysql_fetch_array($result , MYSQL_ASSOC) )
+				{
+					$Linha = $Auxilio;
+					$Linha = str_replace('<%ID%>',$rs['idproduto'],$Linha);
+					$Linha = str_replace('<%IDLOJA%>', $rs['idloja'], $Linha);
+					$Linha = str_replace('<%NOME%>',$rs['nome'],$Linha);
+					$Linha = str_replace('<%CATEGORIA%>',$rs['categoria'],$Linha);
+					$Linha = str_replace('<%SUBCATEGORIA%>', $rs['subcategoria']);
+					$Linha = str_replace('<%IMAGEM%>',$rs['imagem']);
+					$Linha = str_replace('<%PRECO%>',$rs['preco']);
+					$Linha = str_replace('<%LINK%>',$rs['link']);
+					var_dump($Linha);
+					$Produtos .= $Linha;
+				}
+			}else{
+				
+			}
+			return $Produtos;
+		}
+		
+		
 	}
--- shopping/lib/inicio.php
+++ shopping/lib/inicio.php
@@ -6,7 +6,12 @@
 	#Instancia objeto que vai tratar o banco de dados dessa pagina
 	$banco = new bancoinicio();
 	
+	#monta o auxilio para listar os produtos
+	$Auxilio = $banco->CarregaHtml('itens/lista-produto-itens');
+	
+	$Produtos = $banco->ListaProdutosInicio($Auxilio);
 	
 	#Imprime tela
 	$Conteudo = $banco->CarregaHtml('inicio');
+	$Conteudo = str_replace('<%PRODUTOS%>',$Produtos,$Conteudo);
 ?>
