7df9f68b47690661cf86964d29e3e594cc6aec97
Koiti - Note <evandrokoiti@gmail.com>
2013-04-24 19:37:11 -0300

    pesquisar OK

--- shopping/functions/banco-pesquisar.php
+++ shopping/functions/banco-pesquisar.php
@@ -8,12 +8,16 @@
 				$busca = '';
 			}
 			#Query Busca Produtos
-			$Sql = "SELECT P.*,L.nome AS nomeloja FROM s_produto P
-					INNER JOIN s_loja L on P.idloja = L.idloja
+			$Sql = "SELECT P . * , L.nome AS nome_loja, S.nome AS nome_sub, C.nome AS nome_cat
+					FROM s_produtos P
+					INNER JOIN s_lojas L ON P.idloja = L.idloja
+					INNER JOIN fixo_subcategorias S ON P.idsubcategoria = S.idsubcategoria
+					INNER JOIN fixo_categorias C ON S.idcategoria = C.idcategoria
 					WHERE (P.nome LIKE '%".$busca."%' 
-							OR P.categoria LIKE '%".$busca."%' 
-							OR P.subcategoria LIKE '%".$busca."%'
+							OR C.nome LIKE '%".$busca."%' 
+							OR S.nome LIKE '%".$busca."%'
 						   )
+					ORDER BY P.nome
 					";
 			$result = parent::Execute($Sql);
 			$num_rows = parent::Linha($result);
@@ -25,12 +29,12 @@
 					$Linha = str_replace('<%ID%>',$rs['idproduto'],$Linha);
 					$Linha = str_replace('<%IDLOJA%>', $rs['idloja'], $Linha);
 					$Linha = str_replace('<%NOME%>',$rs['nome'],$Linha);
-					$Linha = str_replace('<%CATEGORIA%>',$rs['categoria'],$Linha);
-					$Linha = str_replace('<%SUBCATEGORIA%>', $rs['subcategoria'],$Linha);
+					$Linha = str_replace('<%CATEGORIA%>',$rs['nome_cat'],$Linha);
+					$Linha = str_replace('<%SUBCATEGORIA%>', $rs['nome_sub'],$Linha);
 					$Linha = str_replace('<%IMAGEM%>',$rs['imagem'],$Linha);
 					$Linha = str_replace('<%PRECO%>',$rs['preco'],$Linha);
 					$Linha = str_replace('<%LINK%>',$rs['link'],$Linha);
-					$Linha = str_replace('<%NOMELOJA%>',$rs['nomeloja'],$Linha);
+					$Linha = str_replace('<%NOMELOJA%>',$rs['nome_loja'],$Linha);
 					$Pesquisa .= $Linha;
 				}
 			}else{
