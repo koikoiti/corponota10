204cb45a73d4a18124e45f04e0ef940e70925667
Israel - Note <israelcbj@gmail.com>
2013-04-24 19:48:15 -0300

    fix listando produto unico

--- shopping/lib/produto.php
+++ shopping/lib/produto.php
@@ -7,6 +7,15 @@
 	#Instancia objeto que vai tratar o banco de dados dessa pagina
 	$banco = new bancoproduto;
 	
+	#monta o auxilio para listar os produtos
+	$Auxilio = $banco->CarregaHtml('itens/lista-produto-itens');
+	
+	if($this->PaginaAux[0]){
+		$idproduto = $this->PaginaAux[0];
+		$Produto = $banco->ListaProduto($Auxilio,$idproduto);
+	}
+	
 	#Imprime Valores
 	$Conteudo = $banco->CarregaHtml('produto');
+	$Conteudo = str_replace('<%PRODUTO%>',$Produto,$Conteudo);
 ?>
