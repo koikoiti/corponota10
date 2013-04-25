9e5fe740bcd2024bfdae8d6c8fa1dbafed634925
Israel - Note <israelcbj@gmail.com>
2013-04-25 18:38:37 -0300

    tela de loja buscando produtos

--- shopping/functions/banco-loja.php
+++ shopping/functions/banco-loja.php
@@ -1,8 +1,39 @@
 <?php
-
 	class bancoloja extends banco{
 		
-		
-		
+	function ListaProdutosLoja($Auxilio,$idloja){
+			$Banco_Vazio = "Banco esta Vazio";
+			#Query Busca Docs
+			$Sql = "SELECT P . * , L.nome AS nome_loja, S.nome AS nome_sub, C.nome AS nome_cat
+					FROM s_produtos P
+					INNER JOIN s_lojas L ON P.idloja = L.idloja
+					INNER JOIN fixo_subcategorias S ON P.idsubcategoria = S.idsubcategoria
+					INNER JOIN fixo_categorias C ON S.idcategoria = C.idcategoria
+					where P.idloja = '".$idloja."'
+					";
+			$result = parent::Execute($Sql);
+			$num_rows = parent::Linha($result);
+			
+			#Monta no Html a Listagem
+			if ($num_rows){
+				while( $rs = mysql_fetch_array($result , MYSQL_ASSOC) )
+				{
+					$Linha = $Auxilio;
+					$Linha = str_replace('<%ID%>',$rs['idproduto'],$Linha);
+					$Linha = str_replace('<%IDLOJA%>', $rs['idloja'], $Linha);
+					$Linha = str_replace('<%NOME%>',$rs['nome'],$Linha);
+					$Linha = str_replace('<%CATEGORIA%>',$rs['nome_cat'],$Linha);
+					$Linha = str_replace('<%SUBCATEGORIA%>', $rs['nome_sub'],$Linha);
+					$Linha = str_replace('<%IMAGEM%>',$rs['imagem'],$Linha);
+					$Linha = str_replace('<%PRECO%>',$rs['preco'],$Linha);
+					$Linha = str_replace('<%LINK%>',$rs['link'],$Linha);
+					$Linha = str_replace('<%NOMELOJA%>',$rs['nome_loja'],$Linha);
+					$Produtos .= $Linha;
+				}
+			}else{
+				
+			}
+			return $Produtos;
+		}
 	}
 ?>
--- shopping/lib/loja.php
+++ shopping/lib/loja.php
@@ -7,6 +7,12 @@
 	#Instancia objeto que vai tratar o banco de dados dessa pagina
 	$banco = new bancoloja;
 	
+	$Auxilio = $banco->CarregaHtml('itens/lista-produto-itens');
+	$Produtos = $banco->ListaProdutosLoja($Auxilio,$this->PaginaAux[0]);
+	
 	#Imprime Valores
 	$Conteudo = $banco->CarregaHtml('loja');
+	$Conteudo = str_replace('<%URLPADRAO%>',UrlPadrao,$Conteudo);
+	$Conteudo = str_replace('<%PRODUTOS%>',$Produtos,$Conteudo);
+	
 ?>
