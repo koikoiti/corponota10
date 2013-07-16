5a844c1640b710d84f5bd048316691e7f100f623
Koiti - Note <evandrokoiti@gmail.com>
2013-07-16 19:33:59 -0300

    config

--- shopping/conf/tags.php
+++ shopping/conf/tags.php
@@ -4,8 +4,8 @@
 	define('UrlPadrao' , "http://localhost/corponota10/shopping");
 	
 	#Definições do Banco de Dados
-	//define('DB_Host' , "192.168.0.10");
-	define('DB_Host' , "localhost");
+	define('DB_Host' , "192.168.0.10");
+	//define('DB_Host' , "localhost");
 	define('DB_Database' , "a3786969_cn10");
 	define('DB_User' , "root");
 	define('DB_Pass' , "root");
