cf10c3c5f76ed6ffe58bc93d910d221225275fe5
Israel - Note <israelcbj@gmail.com>
2013-07-22 21:33:51 -0300

    fix links

--- shopping/conf/tags.php
+++ shopping/conf/tags.php
@@ -1,11 +1,17 @@
 <?php
 	#Definições do Sistema
 	date_default_timezone_set('America/Sao_Paulo');
-	define('UrlPadrao' , "http://www.corponota10.com.br/shopping");
-	define('UrlModelo', "http://www.corponota10.com.br");
+	
+	#Quando for enviar para web
+	#define('UrlPadrao' , "http://www.corponota10.com.br/shopping");
+	#define('UrlModelo', "http://www.corponota10.com.br");
+	
+	#Quando usar localhost
+	define('UrlPadrao' , "http://localhost/corponota10/shopping");
+	define('UrlModelo', "http://localhost/corponota10/");
 	
 	#Definições do Banco de Dados
-	define('DB_Host' , "mysql.hostinger.com.br");
+	define('DB_Host' , "192.168.0.10");
 	//define('DB_Host' , "localhost");
 	define('DB_Database' , "u528725906_cn10");
 	define('DB_User' , "u528725906_cn10");
--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Configurações do MySQL - Você pode pegar essas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define('DB_NAME', 'a3786969_cn10');
+define('DB_NAME', 'u528725906_cn10');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'root');
+define('DB_USER', 'u528725906_cn10');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 's1stem@');
 
 /** nome do host do MySQL */
 //define('DB_HOST', 'localhost');
