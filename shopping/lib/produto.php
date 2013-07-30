f1d030b035a3d49c5895aaf215ec7480eea1e916
Israel - Note <israelcbj@gmail.com>
2013-07-30 20:48:22 -0300

    fix produto similar... arrumar css

--- shopping/lib/produto.php
+++ shopping/lib/produto.php
@@ -9,6 +9,7 @@
 	
 	#monta o auxilio para listar os produtos
 	$Auxilio = $banco->CarregaHtml('itens/lista-produto-itens');
+	$AuxilioUnico = $banco->CarregaHtml('itens/lista-produto-unico');
 	
 	if($this->PaginaAux[0] == 'redi'){
 		$link = $this->PaginaAux[2];
@@ -17,10 +18,14 @@
 	
 	if($this->PaginaAux[0]){
 		$idproduto = $this->PaginaAux[0];
-		$Produto = $banco->ListaProduto($Auxilio,$idproduto);
+		$Produto = $banco->ListaProduto($AuxilioUnico,$idproduto);
+		#Lista Produtos Similares
+		$idsubcategoria = $banco->SubCategoriaProduto($idproduto);
+		$ProdutoSimilar = $banco->ListaProdutoSimilar($Auxilio, $idproduto, $idsubcategoria);
 	}
 	
 	#Imprime Valores
 	$Conteudo = $banco->CarregaHtml('produto');
 	$Conteudo = str_replace('<%PRODUTO%>',$Produto,$Conteudo);
+	$Conteudo = str_replace('<%PRODUTOSIMILAR%>',$ProdutoSimilar,$Conteudo);
 ?>
