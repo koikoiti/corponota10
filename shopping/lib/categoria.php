e441cc2298ddf8f17411241a7f782c0f7d0d24fd
Koiti - Note <evandrokoiti@gmail.com>
2013-07-31 22:01:48 -0300

    paginacao OK

--- shopping/lib/categoria.php
+++ shopping/lib/categoria.php
@@ -1,4 +1,20 @@
 <?php
+
+/*
+ * 
+ * 	$pagina = 1;
+		#Coloca na variável a categoria que quer buscar
+		$categoria = $this->PaginaAux[0];
+		$where = "WHERE C.nome = '" . $categoria."'";
+		if(strlen($this->PaginaAux[1]) < 6){
+			$pagina = strstr($this->PaginaAux[1], "pg-");
+			$pagina = str_replace("pg-", "", $pagina);
+		}else{
+			$subcategoria = $this->PaginaAux[1];
+			$where = "WHERE S.nome = '" . $subcategoria."'".
+					" AND C.nome = '" . $categoria."'";
+		}
+	}*/
 	#Declara Variaveis
 	
 	#Include nas funcoes do documento
@@ -10,21 +26,20 @@
 	#monta o auxilio para listar os produtos
 	$Auxilio = $banco->CarregaHtml('itens/lista-produto-itens');
 	
-	if($this->PaginaAux[0]){
-		#Coloca na variável a categoria que quer buscar
-		$categoria = $this->PaginaAux[0];
-		$where = "WHERE C.nome = '" . $categoria."'";
-	}
-	if($this->PaginaAux[1]){
-		$categoria = $this->PaginaAux[0];
-		$subcategoria = $this->PaginaAux[1];
-		$where = "WHERE S.nome = '" . $subcategoria."'".
-				 " AND C.nome = '" . $categoria."'";
+	#Pega a página
+	$pagina = $banco->PaginaCategoria($this->PaginaAux);
+	
+	#Lista a categoria
+	if($this->PaginaAux[1] == "pg" || isset($this->PaginaAux[1])){
+		$cat = $this->PaginaAux[0];
+		$Categoria = $banco->ListaCategoria($Auxilio, $pagina, $cat);
+		$Paginacao = $banco->MontaPaginacaoCat($pagina, $cat);
 	}
 	
-	$Categoria = $banco->ListaCategoria($Auxilio, $where);
 	
+		
 	#Imprime Valores
 	$Conteudo = $banco->CarregaHtml('categoria');
 	$Conteudo = str_replace('<%CATEGORIA%>',$Categoria,$Conteudo);
+	$Conteudo = str_replace('<%PAGINACAO%>',$Paginacao,$Conteudo);
 ?>
